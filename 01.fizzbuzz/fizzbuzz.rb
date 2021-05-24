f9be0ed6a51280805a26711cf8a836047e4ecf33
Umechaso <81458868+umechaso0520@users.noreply.github.com>
2021-05-24 18:53:51 +0900

    add fizzbuzz

---
+++ 01.fizzbuzz/fizzbuzz.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+number = 1
+while number <= 20 do
+  if number % 3 == 0 && number % 5 != 0
+    puts "Fizz"
+  elsif number % 5 == 0 && number % 3 != 0
+    puts "Buzz"
+  elsif number % 3 == 0 && number % 5 == 0
+    puts "FizzBuzz"
+  else
+    puts number
+  end
+  number += 1
+end
+
