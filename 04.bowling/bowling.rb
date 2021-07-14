a64e12b8d3ccfc45f15094febcc25b826d4cd4af
Umechaso <81458868+umechaso0520@users.noreply.github.com>
2021-07-14 16:53:45 +0900

    スコア計算ロジックの簡略化

--- 04.bowling/bowling.rb
+++ 04.bowling/bowling.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
+
 score = ARGV[0]
 scores = score.split(',')
 shots = []
@@ -19,35 +20,25 @@ end
 frames[-2].concat(frames[-1])
 frames.delete_at(-1)
 
-point = 0
-frames.each_with_index do |frame, index|
-  point += if index < 8 # 8ゲーム目までの処理
-             if frame[0] == 10 # ストライクの場合
-               if frames[index + 1][0] == 10
-                 10 + 10 + frames[index + 2][0]
-               else
-                 10 + frames[index + 1][0] + frames[index + 1][1]
-               end
-             elsif frame[0] != 10 && frame.sum == 10 # スペアの場合
-               10 + frames[index + 1][0]
-             else
-               frame.sum
-             end
-           elsif index == 8 # 9ゲーム目の処理
-             if frame[0] == 10
-               if frames[index + 1][0] == 10
-                 10 + 10 + frames[index + 1][1]
-               else
-                 10 + frames[index + 1][0] + frames[index + 1][1]
-               end
-             elsif frame[0] != 10 && frame.sum == 10 # スペアの場合
-               10 + frames[index + 1][0]
-             else
-               frame.sum
-             end
-           else
-             frame.sum
-           end
+point = frames.each_with_index.sum do |frame, index|
+  if index < 9 # 9ゲーム目までの処理
+    if frame[0] == 10 # ストライクの場合
+      if frames[index + 1][0] == 10
+        if index < 8
+          10 + 10 + frames[index + 2][0]
+        else
+          10 + 10 + frames[index + 1][1]
+        end
+      else
+        10 + frames[index + 1][0] + frames[index + 1][1]
+      end
+    elsif frame[0] != 10 && frame.sum == 10 # スペアの場合
+      10 + frames[index + 1][0]
+    else
+      frame.sum
+    end
+  else
+    frame.sum
+  end
 end
-
 puts point
