daf508cb11b33eb6c2899ded1515fb8b2595c8a1
Umechaso <81458868+umechaso0520@users.noreply.github.com>
2022-01-16 07:47:01 +0900

    変数の修正を反映

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,19 +1,17 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-files = Dir.glob('*').sort
-foldback = 3
-row = (files.size.to_f / foldback).ceil
-max_length_word = files.map(&:length).max
+FILES = Dir.glob('*').sort
+FOLDBACK = 3
+number_of_lines = (FILES.size.to_f / FOLDBACK).ceil
+maximum_word_length = FILES.map(&:length).max
 
-file_separation = []
-files.each_slice(row) { |element| file_separation << element }
+separated_files = []
+FILES.each_slice(number_of_lines) { |file| separated_files << file }
 
-row.times do |count|
-  file_separation.each do |i|
-    next if i[count].nil?
-
-    print i[count].ljust(max_length_word + 4)
+number_of_lines.times do |line|
+  separated_files.each do |file|
+    print file[line].ljust(maximum_word_length + 4) unless file[line].nil?
   end
   print "\n"
 end
