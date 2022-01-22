d48370aae5d543c3eec2e5528d34820bb3516331
Umechaso <81458868+umechaso0520@users.noreply.github.com>
2022-01-23 07:28:37 +0900

    lsコマンド2の提出

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,16 +1,26 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-file_names = Dir.glob('*').sort
+require 'optparse'
 FOLDBACK = 3
-line_count = (file_names.size.to_f / FOLDBACK).ceil
-max_word_length = file_names.map(&:length).max
+opt = ARGV.getopts('a')
 
-separated_files = file_names.each_slice(line_count).to_a
-
-line_count.times do |line_index|
-  separated_files.each do |files|
-    print files[line_index].ljust(max_word_length + 4) unless files[line_index].nil?
+def show_file_names(file_names)
+  line_count = (file_names.size.to_f / FOLDBACK).ceil
+  max_word_length = file_names.map(&:length).max
+  separated_files = file_names.each_slice(line_count).to_a
+  line_count.times do |line_index|
+    separated_files.each do |files|
+      print files[line_index].ljust(max_word_length + 4) unless files[line_index].nil?
+    end
+    print "\n"
   end
-  print "\n"
+end
+
+if opt['a'] == true
+  all_file_names = Dir.glob('*', File::FNM_DOTMATCH).sort
+  show_file_names(all_file_names)
+else
+  file_names = Dir.glob('*').sort
+  show_file_names(file_names)
 end
